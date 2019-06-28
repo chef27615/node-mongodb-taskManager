7024e00234cba9513da8e472d84927be8daba8d8
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-28 10:50:00 -0400

    add folders for mongoose

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ const port =process.env.PORT || 3000
 
 app.use(express.json());
 
-
 app.post('/users', (req, res) => {
     const user = new User(req.body)
     user.save().then(()=> {
-        res.send(user)
+        res.status(201).send(user)
     }).catch(err => {
         res.status(400).send(err)
     })
@@ -21,13 +20,31 @@ app.post('/users', (req, res) => {
 app.post('/tasks', (req, res) => {
     const task = new Tasks(req.body)
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch(err => {
         res.status(400).send(err)
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.status(200).send(users)
+    }).catch(e => {
+        res.status(500).send()
+    })
+})
 
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+        }).catch(e => {
+        res.status(500).send()
+    })
+})
 
 app.listen(port, () => {
     console.log(`app started on ${port}`)
